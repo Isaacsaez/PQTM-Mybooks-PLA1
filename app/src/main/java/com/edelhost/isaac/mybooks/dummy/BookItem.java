2dec331030651c9be1290d07c82913998012ba79
Isaac <isaacsaezruiz@gmail.com>
2018-10-09 21:23:32 +0200

    8 - Incio CardView

--- app/src/main/java/com/edelhost/isaac/mybooks/dummy/BookItem.java
+++ app/src/main/java/com/edelhost/isaac/mybooks/dummy/BookItem.java
@@ -26,8 +26,10 @@ public class BookItem {
 
    static{
        // Añadimos 2 Libros para la lista
-       addItem(new DummyItem("1","Las Fábulas de las Emociones", "María Valverde", "23-04-2017","Libro de emociones infantil"));
-       addItem(new DummyItem("2","Sisu Yoga", "María Valverde","25-03-2019","Libro de Yoga para niños"));
+       addItem(new DummyItem("1","Las Fábulas de las Emociones", "María Valverde", "23-04-2017","Este libro contiene las claves para aprender a desarrollar la inteligencia emocional. Éstas permitiran al niño identificar y aceptar sus emociones, sin dejarse llevar por ellas, consiguiendo así, una mayor autonomía para resolver situaciones de manera más positiva. Además de ser un apoyo para la adquisición de buenos hábitos tales como: la alimentación, la rutina del sueño y la higiene, entre otros."));
+       addItem(new DummyItem("2","Sisu Yoga", "María Valverde","25-03-2019","Este cuento pretende ser una herramienta para introducir a los niños en el mundo del conocimiento personal, a través de una historia emocionante y divertida, en la que se sentirán los protagonistas.\n" +
+               "\n" +
+               "El contenido del libro es una sesión de yoga que empieza por tomar conciencia del cuerpo, sigue con unas aasanas o ejercicios adaptados a los niños,, que fomentan la flexibilidad, la habilidad y la coordinación y finaliza con un pranayama o respiraciones, que favorecen la concentración."));
    }
 
     private static void addItem(DummyItem item) {
@@ -42,12 +44,15 @@ public class BookItem {
         public String fecha;
         public String descripcion;
 
+
+
         public DummyItem(String id, String titulo, String autor, String fecha, String descripcion) {
             this.id = id;
             this.titulo = titulo;
             this.autor = autor;
             this.fecha = fecha;
             this.descripcion = descripcion;
+
         }
 
         @Override
