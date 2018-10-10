e137e0e7f9c7f1717918fac0ed8ab85bda7fb779
Isaac <isaacsaezruiz@gmail.com>
2018-10-10 10:00:13 +0200

    10 - Práctica 1 ejercicio 1,2 y 3 -> CardView finalizado

--- app/src/main/java/com/edelhost/isaac/mybooks/BookDetailFragment.java
+++ app/src/main/java/com/edelhost/isaac/mybooks/BookDetailFragment.java
@@ -7,44 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.edelhost.isaac.mybooks.dummy.BookItem;
 
-/**
- * A fragment representing a single Book detail screen.
- * This fragment is either contained in a {@link BookListActivity}
- * in two-pane mode (on tablets) or a {@link BookDetailActivity}
- * on handsets.
- */
+
+// Fragment que contendrá los datos del detalle de los libros y será colocada en la actividad
+// BookDetailActivity
+
 public class BookDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
     private BookItem.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public BookDetailFragment() {
-    }
+    //recuperamos la información de los diferentes campos del libro que hemos seleccionado
+    //Incluyendo el Título de la Barra superior
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
+
             mItem = BookItem.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -55,22 +39,21 @@ public class BookDetailFragment extends Fragment {
         }
     }
 
+    // Insertamos la información recuperada de cada campo, en el View correspondiente
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.book_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.tv_titulo)).setText(mItem.titulo);
             ((TextView) rootView.findViewById(R.id.tv_autor)).setText(mItem.autor);
             ((TextView) rootView.findViewById(R.id.tv_fecha)).setText(mItem.fecha);
             ((TextView) rootView.findViewById(R.id.tv_desc)).setText(mItem.descripcion);
-
         }
 
         return rootView;
-
     }
 
 }
--- app/src/main/java/com/edelhost/isaac/mybooks/dummy/BookItem.java
+++ app/src/main/java/com/edelhost/isaac/mybooks/dummy/BookItem.java
@@ -1,42 +1,36 @@
 package com.edelhost.isaac.mybooks.dummy;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
+
+// Clase que contiene los datos de los libros listados en la App MyBooks
+
 public class BookItem {
 
-    /**
-     * An array of sample (dummy) items.
-     */
+
+    // Creamos el array de libros a mostrar y un map del array
     public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
 
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
+    // Añadimos 2 Libros para la lista con sus diferentes campos
    static{
-       // Añadimos 2 Libros para la lista
        addItem(new DummyItem("1","Las Fábulas de las Emociones", "María Valverde", "23-04-2017","Este libro contiene las claves para aprender a desarrollar la inteligencia emocional. Éstas permitiran al niño identificar y aceptar sus emociones, sin dejarse llevar por ellas, consiguiendo así, una mayor autonomía para resolver situaciones de manera más positiva. Además de ser un apoyo para la adquisición de buenos hábitos tales como: la alimentación, la rutina del sueño y la higiene, entre otros."));
        addItem(new DummyItem("2","Sisu Yoga", "María Valverde","25-03-2019","Este cuento pretende ser una herramienta para introducir a los niños en el mundo del conocimiento personal, a través de una historia emocionante y divertida, en la que se sentirán los protagonistas.\n" +
                "\n" +
                "El contenido del libro es una sesión de yoga que empieza por tomar conciencia del cuerpo, sigue con unas aasanas o ejercicios adaptados a los niños,, que fomentan la flexibilidad, la habilidad y la coordinación y finaliza con un pranayama o respiraciones, que favorecen la concentración."));
    }
 
+
     private static void addItem(DummyItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
 
+    // creamos las variables para cada campo de los libros
     public static class DummyItem {
         public String id;
         public String titulo;
